3c4441c5b8a609234f60552f8bc0b3b61d875204
Ivan Tolstosheyev <itroot@yandex-team.ru>
2012-07-20 16:10:37 +0400

    Solve problem #9

--- 9/solve.py
+++ 9/solve.py
@@ -1,5 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import math
+
 tripletSum=1000
+
+def is_square(number):
+    sqrtNumber=int(math.sqrt(number))
+    return sqrtNumber*sqrtNumber==number
+
+def verifyFirstTwoNumbers(first, second):
+     sumOfSquares=first**2+second**2
+     if is_square(sumOfSquares):
+        return True
+
+for first in range (1,1000):
+    for second in range(first, 1000):
+        if verifyFirstTwoNumbers(first, second):
+            third=int(math.sqrt(first**2+second**2))
+            sum=(first+second+third)
+            if 1000==sum:
+                print first*second*third
+
 
