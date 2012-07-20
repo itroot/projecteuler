b004b406046e840cf3eba777e6342b0105a17d4f
Ivan Tolstosheyev <itroot@yandex-team.ru>
2012-07-20 10:43:44 +0400

    Add some thoughts how to solve problem #9

---
+++ 9/solve.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# a^2+b^2=c^2
+# a+b+c=1000
+# c=1000-a-b
+# a^2+b^2=(1000-a-b)^2
+# a^2+b^2=1000000+a^2+b^2+2ab-2000a-2000b
+# ab-1000a-1000b=500000
+
+# Also if we can achieve that a+b+c=n, where n is any factor of 1000,
+# we can simple multiply by 1000/n all number and get the results
+
+tripletSum=1000
+
