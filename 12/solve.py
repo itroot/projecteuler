8c000c099c689a7ff8499232b9c83f53da3d8bec
Ivan Tolstosheyev <itroot@yandex-team.ru>
2012-07-22 21:13:38 +0400

    Solve problem #12

--- 12/solve.py
+++ 12/solve.py
@@ -9,10 +9,27 @@ sys.path.append("../lib")
 from Factorize import *
 
 def numberOfDivisorsFromFactors(factorization):
+    factor2power={}
+    for factor in factorization:
+        if factor in factor2power:
+            factor2power[factor]+=1
+        else:
+            factor2power[factor]=1
+    result=1
+    for factor in factor2power:
+        result*=factor2power[factor]+1
+    return result
 
 factorization=[2]
-for i in range(3, 10):
+for i in range(3, 90000):
+    triangleNumber=i*(i-1)/2
     nextFactorization=factorize(i)
     mergeFactorization=sorted(factorization+nextFactorization)
+    mergeFactorization=mergeFactorization[1:]
+    divisorsNumber=numberOfDivisorsFromFactors(mergeFactorization)
+    #print triangleNumber, mergeFactorization, divisorsNumber
+    if (divisorsNumber>500):
+        print triangleNumber
+        break
     factorization=nextFactorization
 
