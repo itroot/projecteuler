433d0b15745aa0274572fee90da99e353b016c3b
Ivan Tolstosheyev <itroot@yandex-team.ru>
2012-07-23 09:47:54 +0400

    [#14] Add class Sequence

--- 14/solve.py
+++ 14/solve.py
@@ -1,27 +1,29 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-def advanceSequence(number):
-    if (0==number%2):
-        return number/2
-    else:
-        return 3*number+1
-
-#sequence={}
-
-def buildSequence(number):
-    result=[]
-    while 1!=number:
+class Sequence:
+    def __init__(self):
+        self.__cache={2:1}
+    def __advance(self, number):
+        if (0==number%2):
+            return number/2
+        else:
+            return 3*number+1
+    def build(self, number):
+        result=[]
+        while 1!=number:
 #        if number in sequence:
 #            result.append(sequence[number])
 #            return result
 #        else:
-        result.append(number)
-        number=advanceSequence(number)
-    result.append(1)
+            result.append(number)
+            number=self.__advance(number)
+        result.append(1)
 #    sequence[number]=result
-    return result
+        return result
 
-for i in range(1, 1000):
-    sequence=buildSequence(i)
-    print len(sequence), sequence
+
+sequence=Sequence()
+for i in range(1, 10):
+    s=sequence.build(i)
+    print len(s), s
