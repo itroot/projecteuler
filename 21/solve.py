8d5ac8fe4942b69f9c20af91356de868796a6b29
Ivan Tolstosheyev <itroot@yandex-team.ru>
2012-07-25 23:18:10 +0400

    [#21] Start writing code for problem

---
+++ 21/solve.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+sys.path.append("../lib")
+from Factorize import *
+
+def getAllDivisors(number):
+    factors=factorize(number)
+    s=set(factors)
+    s.add(1)
+    length=len(set)
+    while 
+    return factors
+
+print getAllDivisors(220)
