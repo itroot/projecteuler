0dcaa04406754a7157e99f53f218938bebecf5df
Ivan Tolstosheyev <itroot@yandex-team.ru>
2012-07-26 20:20:17 +0400

    [#24] nth_permutation is working on [0, 1, 2]

--- 24/solve.py
+++ 24/solve.py
@@ -18,15 +18,19 @@ digits=range(0, upperLimit+1)
 def nth_permutation(digits, number):
     length=len(digits)
     factorial=math.factorial(length)
+    prevFactorial=math.factorial(length-1)
     if (number<0 or number>=factorial):
         raise Exception("Incorrect parameters, digits: %s, number: %d" % (str(digits), number))
     if (0==number):
         return digits
-    if (number<=factorial):
+    if (number<prevFactorial):
         return [digits[0]]+nth_permutation(digits[1:], number)
     else:
-        divide_result=number//(length)
-        reminder=number%(length)
+        divide_result=number//(prevFactorial)
+        reminder=number%(prevFactorial)
+        newDigits=list(digits)
+        newDigits[0], newDigits[divide_result]=newDigits[divide_result], newDigits[0]
+        return [newDigits[0]]+nth_permutation(newDigits[1:], reminder)
 
 for i in range(0, math.factorial(upperLimit+1)):
     print nth_permutation(digits, i)
