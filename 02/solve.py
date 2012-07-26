5f1cc74efed99f6abc365d877fe1011b69eda9d4
Ivan Tolstosheyev <itroot@yandex-team.ru>
2012-07-26 20:29:41 +0400

    [#02] Move class Fibonacci to lib

--- 02/solve.py
+++ 02/solve.py
@@ -1,17 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-class Fibonacci:
-    def __init__(self):
-        self.__cache=[0, 1]
-    def __PopulateCacheUntil(self, position):
-        length=len(self.__cache)
-        for i in range(length, position+1):
-            self.__cache.append(self.__cache[i-1]+self.__cache[i-2])
-    def GetNumberAtPosition(self, position):
-        if (position>=len(self.__cache)):   
-            self.__PopulateCacheUntil(position)
-        return self.__cache[position]
+import sys
+sys.path.append("../lib")
+from Fibonacci import *
 
 MAX_FIBONACCI=4000000
 fibonacci=Fibonacci()
---
+++ lib/Fibonacci.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+class Fibonacci:
+    def __init__(self):
+        self.__cache=[0, 1]
+    def __PopulateCacheUntil(self, position):
+        length=len(self.__cache)
+        for i in range(length, position+1):
+            self.__cache.append(self.__cache[i-1]+self.__cache[i-2])
+    def GetNumberAtPosition(self, position):
+        if (position>=len(self.__cache)):   
+            self.__PopulateCacheUntil(position)
+        return self.__cache[position]
