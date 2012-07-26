6c7ddefb0b15cd445ceff8672fd80793b98fbb35
Ivan Tolstosheyev <itroot@yandex-team.ru>
2012-07-26 12:05:35 +0400

    [#21] Move class AllDivisors to Factorize.py

--- 21/solve.py
+++ 21/solve.py
@@ -5,31 +5,6 @@ import sys
 sys.path.append("../lib")
 from Factorize import *
 
-class AllDivisors:
-    def __init__(self, factorization):
-        self.__powers=factorization2power(factorization)
-        self.__uniqueSortedFactors=sorted(self.__powers)
-        self.__divisors=[]
-        self.__generate(1, 0)
-        self.__divisors=sorted(self.__divisors)
-    def __generate(self, value, uniqueFactorNum):
-        if (uniqueFactorNum==len(self.__uniqueSortedFactors)):
-            self.__divisors.append(value)
-            return
-        factor=self.__uniqueSortedFactors[uniqueFactorNum]
-        power=self.__powers[factor]
-        for p in range(0, power+1):
-            self.__generate(value*factor**p,uniqueFactorNum+1)
-    def divisors(self):
-        return self.__divisors
-
-def getAllDivisors(number):
-    if (1==number):
-        return [1]
-    factors=factorize(number)
-    allDivisors=AllDivisors(factors)
-    return allDivisors.divisors()
-
 def isAmicable(number):
     other=sum(getAllDivisors(number)[:-1])
     if (0==other):
--- lib/Factorize.py
+++ lib/Factorize.py
@@ -30,3 +30,28 @@ def factorization2power(factorization):
         else:
             result[factor]=1
     return result
+
+class AllDivisors:
+    def __init__(self, factorization):
+        self.__powers=factorization2power(factorization)
+        self.__uniqueSortedFactors=sorted(self.__powers)
+        self.__divisors=[]
+        self.__generate(1, 0)
+        self.__divisors=sorted(self.__divisors)
+    def __generate(self, value, uniqueFactorNum):
+        if (uniqueFactorNum==len(self.__uniqueSortedFactors)):
+            self.__divisors.append(value)
+            return
+        factor=self.__uniqueSortedFactors[uniqueFactorNum]
+        power=self.__powers[factor]
+        for p in range(0, power+1):
+            self.__generate(value*factor**p,uniqueFactorNum+1)
+    def divisors(self):
+        return self.__divisors
+
+def getAllDivisors(number):
+    if (1==number):
+        return [1]
+    factors=factorize(number)
+    allDivisors=AllDivisors(factors)
+    return allDivisors.divisors()
