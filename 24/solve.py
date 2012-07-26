bb8b2445b7414359e383a7aa2a9d0b16c719f74f
Ivan Tolstosheyev <itroot@yandex-team.ru>
2012-07-26 20:21:50 +0400

    [#24] Simplify code

--- 24/solve.py
+++ 24/solve.py
@@ -23,14 +23,11 @@ def nth_permutation(digits, number):
         raise Exception("Incorrect parameters, digits: %s, number: %d" % (str(digits), number))
     if (0==number):
         return digits
-    if (number<prevFactorial):
-        return [digits[0]]+nth_permutation(digits[1:], number)
-    else:
-        divide_result=number//(prevFactorial)
-        reminder=number%(prevFactorial)
-        newDigits=list(digits)
-        newDigits[0], newDigits[divide_result]=newDigits[divide_result], newDigits[0]
-        return [newDigits[0]]+nth_permutation(newDigits[1:], reminder)
+    divide_result=number//(prevFactorial)
+    reminder=number%(prevFactorial)
+    newDigits=list(digits)
+    newDigits[0], newDigits[divide_result]=newDigits[divide_result], newDigits[0]
+    return [newDigits[0]]+nth_permutation(newDigits[1:], reminder)
 
 for i in range(0, math.factorial(upperLimit+1)):
     print nth_permutation(digits, i)
