6831a2a5414ca68e443567c14871fe900786709f
Ivan Tolstosheyev <itroot@yandex-team.ru>
2012-07-26 10:59:50 +0400

    [#21] Solve problem

--- 21/solve.py
+++ 21/solve.py
@@ -24,8 +24,27 @@ class AllDivisors:
         return self.__divisors
 
 def getAllDivisors(number):
+    if (1==number):
+        return [1]
     factors=factorize(number)
     allDivisors=AllDivisors(factors)
     return allDivisors.divisors()
 
-print getAllDivisors(220)
+def isAmicable(number):
+    other=sum(getAllDivisors(number)[:-1])
+    if (0==other):
+        return False
+    otherSum=sum(getAllDivisors(other)[:-1])
+    return number==otherSum
+
+amicables=set()
+for i in range(1, 10000):
+    if isAmicable(i):
+        opposite=sum(getAllDivisors(i)[:-1])
+        if i!=opposite:
+            amicables.add(tuple(sorted([i, opposite])))
+
+result=0
+for amicable in amicables:
+    result+=sum(amicable)
+print result
