5da8969a47d56d1847cc2cdf3cd00305fb2f00ba
Ivan Tolstosheyev <itroot@yandex-team.ru>
2012-08-02 11:10:26 +0400

    [lib] Improve sieve by limiting dividor on sqrt of number

--- lib/EratosthenesSieve.py
+++ lib/EratosthenesSieve.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import math
+
 class EratosthenesSieve:
     def __init__(self):
         self.__sieve=[2]
     def sieve(self):
         return self.__sieve
     def __isFactoringWithSieve(self, number):
+        numberSqrtPlusOne=int(math.ceil(math.sqrt(number)))+1
         for prime in self.__sieve:
+            if (prime>numberSqrtPlusOne):
+                return False
             if (0==number%prime):
                 return True
         return False
