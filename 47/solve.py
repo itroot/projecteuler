5975d0533aed77cdecc720d453195a132e745728
Ivan Tolstosheyev <itroot@yandex-team.ru>
2012-08-05 23:36:57 +0400

    [#47] Add too long solution, didn't get the answer

--- 47/solve.py
+++ 47/solve.py
@@ -3,14 +3,22 @@
 
 import sys
 sys.path.append("../lib")
-from EratosthenesSieve import *
-import bisect
-import math
+from Factorize import *
 
 upperLimit=100000
+length=4
 
-sieve=EratosthenesSieve()
-sieve.growToNumber(upperLimit)
-primeList=sieve.sieve()
-primesSet=set(primeList)
-
+for i in range(2, upperLimit):
+    numbers=range(i, i+length)
+    factorizations=map(lambda e: set(factorize(e)), numbers)
+    if not reduce(lambda a, b: a and (len(b)==length), factorizations, True):
+        continue
+    #if min(map(lambda e: len(e), factorizations))==1:
+    #    continue
+    #a1=sum(map(lambda e: len(set(e)), factorizations))
+    #a2=len(reduce(lambda a, b: a.union(b), factorizations))
+    #print i, factorizations, a1, a2
+    #if a1==a2:
+    else:
+        print i, factorizations
+        break
