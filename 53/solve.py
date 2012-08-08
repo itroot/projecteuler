e48ec7ee45ecf8c607d41caf31776ae1ad2d627b
Ivan Tolstosheyev <itroot@yandex-team.ru>
2012-08-08 22:00:33 +0400

    [#53] Solve problem

--- 53/solve.py
+++ 53/solve.py
@@ -1,3 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import math
+
+upperLimit=100
+minC=10**6
+
+def C(n, k):
+    f=math.factorial
+    if n<k:
+        raise Exception("n<k")
+    return f(n)/(f(k)*f(n-k))
+
+#print C(5, 3)
+count=0
+for n in range(0, upperLimit+1):
+    for k in range(0, n+1):
+	if C(n, k)>minC:
+            count+=1
+print count
