8b318d6b4b3dd07b2c86a82aaa8bc24a16f14873
Ivan Tolstosheyev <itroot@yandex-team.ru>
2012-08-11 00:41:12 +0400

    [#54] Add solve.py

---
+++ 54/solve.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+letter2suit={
+    "S" : "Spades"
+,   "H" : "Hearts"
+,   "D" : "Diamonds"
+,   "C" : "Clubs"
+}
+
+letter2name={
+}
+
+class Card:
+    def __init__(self, abbreviation):
+        pass
