f05da4deb9359d5530a78191953ee0570b1b2f6f
Ivan Tolstosheyev <itroot@yandex-team.ru>
2012-08-18 17:51:30 +0400

    [#59] Caclulate frequences of letters

--- in-progress/59/solve.py
+++ in-progress/59/solve.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# http://en.wikipedia.org/wiki/Letter_frequency
+
 codeWordLength=3
-encodedAsciiText=open("cipher1.txt").read().rstrip("\r\n").split(",")
-#print encodedAsciiText
+
+def deXor(result, participant):
+    for i in range(0, 255):
+        if result==participant^i:
+            return i
+    raise Exception("Can't deXor %d" % participant)
+
+content=open("cipher1.txt").read().rstrip("\r\n")
+encodedAsciiText=map(lambda e: int(e), content.split(","))
+#print encodedAsciiText, len(encodedAsciiText)
 
 letter2RateList=[{} for i in range(0, codeWordLength)]
 for (i, letter) in enumerate(encodedAsciiText):
@@ -13,8 +23,20 @@ for (i, letter) in enumerate(encodedAsciiText):
         letter2rate[letter]=0
     letter2rate[letter]+=1
 
-for letter2rate in letter2RateList:
-    rates=sorted(letter2rate.iteritems(), key=lambda e: e[1])
-    print rates
-    print
+possibleTopLetters=["e", "e", "e"]
+cypherLetterList=[]
 
+for (i, letter2rate) in enumerate(letter2RateList):
+    rates=sorted(letter2rate.iteritems(), key=lambda e: e[1], reverse=True)
+    print rates
+    topLetter=possibleTopLetters[i]
+    cypherLetterList.append(chr(deXor(ord(topLetter), rates[0][1])))
+print cypherLetterList
+
+decodedAsciiText=[]
+for (i, letter) in enumerate(encodedAsciiText):
+    decodedAsciiText.append(letter^ord(cypherLetterList[i%codeWordLength]))
+
+print "="*80
+print "".join(map(lambda e: chr(e), decodedAsciiText))
+print "="*80
