414a743a8bc53e319e45b3e031d5a7aeece6d988
Ivan Tolstosheyev <itroot@yandex-team.ru>
2012-08-18 13:58:39 +0400

    [59] Add untested rate calculation method

--- in-progress/59/solve.py
+++ in-progress/59/solve.py
@@ -1,3 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+codeWordLength=3
+encodedAsciiText=open("cipher1.txt").read().rstrip("\r\n").split(",")
+#print encodedAsciiText
+
+letter2RateList=[{} for i in range(0, codeWordLength)]
+for (i, letter) in enumerate(encodedAsciiText):
+    letter2RateIndex=i%codeWordLength
+    letter2rate=letter2RateList[letter2RateIndex]
+    if not letter in letter2rate:
+        letter2rate[letter]=0
+    letter2rate[letter]+=1
+
+for letter2rate in letter2RateList:
+    rates=sorted(letter2rate.iteritems(), key=lambda e: e[1])
+    print rates
+    print
+
