e4e4af8f81d6b4806f596dd9081b5e42a8d8dda9
Ivan Tolstosheyev <itroot@yandex-team.ru>
2012-11-04 12:00:37 +0400

    Move class Card to separate file

--- in-progress/54/solve.py
+++ in-progress/54/solve.py
@@ -11,18 +11,6 @@ combination2rank=generateCombination2Rank()
 #print letter2rank
 #print combination2rank
 
-class Card:
-    def __init__(self, abbreviation):
-        self.__abbreviation=abbreviation
-        self.__rank=letter2rank[abbreviation[0]]
-        self.__suit=letter2suit[abbreviation[1]]
-    def rank(self):
-        return self.__rank
-    def suit(self):
-        return self.__suit
-    def __repr__(self):
-        return self.__abbreviation
-
 def findPairIndexInCards(cards):
     for i in range(0, len(cards)):
         for j in range(i+1, len(cards)):
