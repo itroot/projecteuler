a93ae33fd43d2c309d7e989840c3b335c04e0dc0
Ivan Tolstosheyev <itroot@yandex-team.ru>
2012-11-04 11:58:10 +0400

    Move functions to UnsortedFunctionality.py

--- in-progress/54/solve.py
+++ in-progress/54/solve.py
@@ -1,48 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
-def generateLetter2Suit():
-    suits=["Spades", "Hearts", "Diamonds", "Clubs"]
-    result={}
-    for suit in suits:
-        result[suit[0]]=suit
-    return result
+from HighCard import *
 
 letter2suit=generateLetter2Suit()
-
-
-def generateLetter2Rank():
-    lettersSmall=map(lambda e: chr(e), range(ord("2"), ord("9")+1))
-    lettersBig=["T", "J", "Q", "K", "A"]
-    letters=lettersSmall+lettersBig
-    result={}
-    for (i, letter) in enumerate(letters):
-        result[letter]=i+2
-    return result
-
 letter2rank=generateLetter2Rank()
-
-#print letter2rank
-
-def generateCombination2Rank():
-    combinationNames=[
-      "HighCard"
-    , "OnePair"
-    , "TwoPairs"
-    , "ThreeOfAKind"
-    , "Straight"
-    , "Flush"
-    , "FullHouse"
-    , "FourOfAKind"
-    , "StraightFlush"
-    , "RoyalFlush"
-    ]
-    result={}
-    for (i, combinationName) in enumerate(combinationNames):
-        result[combinationName]=i
-    return result
-
 combination2rank=generateCombination2Rank()
 
 #print letter2suit
