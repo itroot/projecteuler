e7b4544d93ee1b0a29bcc841a169f1b4941b8965
Ivan Tolstosheyev <itroot@yandex-team.ru>
2012-11-14 23:08:27 +0400

    [#61] Move code to function

--- in-progress/61/solve.py
+++ in-progress/61/solve.py
@@ -14,14 +14,19 @@ sequences=[
     OctagonalNumber(),
 ]
 
-for sequence in sequences:
+def get4DigitNumbersList():
     result=[]
-    while True:
-        number=sequence.generate()
-        digitsInNumber=len(str(number))
-        if (digitsInNumber>4):
-            break
-        if 4==digitsInNumber:
-            result.append(number)
-        sequence.advance()
-    print result
+    for sequence in sequences:
+        numbers=[]
+        while True:
+            number=sequence.generate()
+            digitsInNumber=len(str(number))
+            if (digitsInNumber>4):
+                break
+            if 4==digitsInNumber:
+                numbers.append(number)
+            sequence.advance()
+        result.append(numbers)
+    return result
+
+print get4DigitNumbersList()
