efa67cbadef3cf9ad26838f37d809d721819251d
Ivan Tolstosheyev <itroot@yandex-team.ru>
2012-11-14 22:21:54 +0400

    [#45] Move sequences generator to SequenceNumber module

---
+++ lib/SequenceNumber.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+class SequenceNumber:
+    def __init__(self):
+        self.__n=0
+    def advance(self):
+        self.__n+=1
+    def n(self):
+        return self.__n
+
+class TriangleNumber(SequenceNumber):
+    def generate(self):
+        n=self.n()
+        return n*(n+1)/2
+
+class PentagonalNumber(SequenceNumber):
+    def generate(self):
+        n=self.n()
+        return n*(3*n-1)/2
+
+class HexagonalNumber(SequenceNumber):
+    def generate(self):
+        n=self.n()
+        return n*(2*n-1)
+
--- solved/level-2/45/solve.py
+++ solved/level-2/45/solve.py
@@ -1,31 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import sys
+sys.path.append("../lib")
+from SequenceNumber import *
+
 solutionKnown=40755
-
-class SequenceNumber:
-    def __init__(self):
-        self.__n=0
-    def advance(self):
-        self.__n+=1
-    def n(self):
-        return self.__n
-
-class TriangleNumber(SequenceNumber):
-    def generate(self):
-        n=self.n()
-        return n*(n+1)/2
-
-class PentagonalNumber(SequenceNumber):
-    def generate(self):
-        n=self.n()
-        return n*(3*n-1)/2
-
-class HexagonalNumber(SequenceNumber):
-    def generate(self):
-        n=self.n()
-        return n*(2*n-1)
-
 sequences=[TriangleNumber(), PentagonalNumber(), HexagonalNumber()]
 
 while True:
