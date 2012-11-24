f4d90db8ca0c523094135c907af1cea5545b35d4
Ivan Tolstosheyev <itroot@yandex-team.ru>
2012-11-24 16:43:08 +0400

    [#66] Add and use workaround functions

--- in-progress/66/solve.py
+++ in-progress/66/solve.py
@@ -7,5 +7,16 @@ import sys
 sys.path.append("../lib")
 from ContinuedFraction import *
 
+def getNotationApproximation(notation, length):
+    (prefix, period)=notation
+    result=[]
+    result+=prefix
+    while (len(result)<length):
+        result+=period
+    return result[:length]
+
 for i in (2, 3, 5, 6, 7):
-    print i, calculateSquareFractionNotation(i)
+    notation=calculateSquareFractionNotation(i)
+    approximation=getNotationApproximation(notation, 10)
+    fraction=calculateFraction(approximation)
+    print i, notation, approximation, fraction
