81aa90f2079b81b6c9c74a5b242e299b4d27289a
Ivan Tolstosheyev <itroot@yandex-team.ru>
2012-11-25 15:11:00 +0400

    [#70] Implement naive method

--- in-progress/70/solve.py
+++ in-progress/70/solve.py
@@ -11,3 +11,10 @@ def eulerTotient(number):
     return reduce(lambda a, b: a*(b-1), uniquePrimeList, 1)
 
 print eulerTotient(87109)
+
+for number in range(2, 10**5):
+    hashSort=lambda s: "".join(sorted([c for c in str(s)]))
+    phi=eulerTotient(number)
+    if hashSort(number)==hashSort(phi):
+        print number, phi, number/(1.0*phi), factorize(number)
+    
