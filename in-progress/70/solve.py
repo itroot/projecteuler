f7ada4b39628d9e1294c19cf5e6f2c452163c3dc
Ivan Tolstosheyev <itroot@yandex-team.ru>
2012-11-25 15:22:19 +0400

    [#70] Add interesting idea

--- in-progress/70/solve.py
+++ in-progress/70/solve.py
@@ -4,6 +4,29 @@
 import sys
 sys.path.append("../lib")
 from Factorize import *
+from EratosthenesSieve import *
+import math
+
+upperLimit=10**7
+
+sieve=EratosthenesSieve()
+sieve.growToNumber(int(math.ceil(math.sqrt(upperLimit))))
+primeList=list(sieve.sieve())
+print primeList, len(primeList)
+
+hashSort=lambda s: "".join(sorted([c for c in str(s)]))
+
+for prime1 in primeList:
+    for prime2 in primeList:
+    #    if prime2<prime1:
+    #        continue
+        number=prime1*prime2
+        phi=(prime1-1)*(prime2-1)
+        #print number, phi
+        if hashSort(number)==hashSort(phi):
+            print "#", number, phi, number/(1.0*phi), factorize(number)
+
+
 
 def eulerTotient(number):
     factorization=factorize(number)
@@ -13,7 +36,6 @@ def eulerTotient(number):
 print eulerTotient(87109)
 
 for number in range(2, 10**5):
-    hashSort=lambda s: "".join(sorted([c for c in str(s)]))
     phi=eulerTotient(number)
     if hashSort(number)==hashSort(phi):
         print number, phi, number/(1.0*phi), factorize(number)
