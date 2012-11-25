e45af7f556f381faf5daf5cceea1c927fc4794ad
Ivan Tolstosheyev <itroot@yandex-team.ru>
2012-11-25 15:51:23 +0400

    [#70] Solve problem

--- solved/level-3/70/solve.py
+++ solved/level-3/70/solve.py
@@ -10,22 +10,30 @@ import math
 upperLimit=10**7
 
 sieve=EratosthenesSieve()
-sieve.growToNumber(int(math.ceil(math.sqrt(upperLimit))))
+sieve.growToNumber(2*int(math.ceil(math.sqrt(upperLimit))))
 primeList=list(sieve.sieve())
-print primeList, len(primeList)
+#print primeList, len(primeList)
 
 hashSort=lambda s: "".join(sorted([c for c in str(s)]))
 
+results=[]
+
 for prime1 in primeList:
     for prime2 in primeList:
-    #    if prime2<prime1:
-    #        continue
+        if prime2<prime1:
+             continue
         number=prime1*prime2
+        if number>upperLimit:
+            continue
         phi=(prime1-1)*(prime2-1)
         #print number, phi
         if hashSort(number)==hashSort(phi):
-            print "#", number, phi, number/(1.0*phi), factorize(number)
+            #print "#", number, phi, number/(1.0*phi), factorize(number)
+            results.append((number, phi, number/(1.0*phi)))
 
+results=sorted(results, key=lambda e: e[2])
+print results[0][0]
+sys.exit(0)
 
 
 def eulerTotient(number):
