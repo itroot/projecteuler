2c1c2ecdd5a0da160af086b32c1e9f0ddcdf94b5
Ivan Tolstosheyev <itroot@yandex-team.ru>
2012-12-02 20:57:58 +0400

    [#73] Add some examples from previous problem

--- in-progress/73/solve.py
+++ in-progress/73/solve.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import sys
+sys.path.append("../lib")
+from Factorize import *
+
 upperLimit=12000
+
+
+f=Factorization()
+print f.eulerTotient(8)
+print sum(f.eulerTotient(i) for i in range(2, 9))
