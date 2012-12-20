57e472a49545bbf2104ad9ed57e0d7a19b42e6cd
Ivan Tolstosheyev <itroot@yandex-team.ru>
2012-12-20 19:51:09 +0400

    [#76] Add calculation of actual numbers

--- in-progress/76/solve.py
+++ in-progress/76/solve.py
@@ -4,14 +4,16 @@
 inputNumber=100
 
 def differentWaysWithMaximum(number, maximum):
-    result=None
+    result=(0, [])
     if 1==maximum:
-        result=1
+        result=(1, [[1 for i in range(0, maximum)]])
     else:
         for i in range(1, maximum+1):
             newNumber=number-i
-            if (newNumber>=0):
-                result+=differentWaysWithMaximum(newNumber, i)
+            if (newNumber>0):
+                numberAndWays=differentWaysWithMaximum(newNumber, i)
+                array=result[1]+[([newNumber]+x) for x in numberAndWays[1]]
+                result=(result[0]+numberAndWays[0], array)
     #print (number, maximum, result)
     return result
 
