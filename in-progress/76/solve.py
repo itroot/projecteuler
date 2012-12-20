ff4a766a50ab67cb0023675a46eb4b85318fbd50
Ivan Tolstosheyev <itroot@yandex-team.ru>
2012-12-20 19:46:24 +0400

    [#76] Add idea, not working right now

--- in-progress/76/solve.py
+++ in-progress/76/solve.py
@@ -3,4 +3,19 @@
 
 inputNumber=100
 
+def differentWaysWithMaximum(number, maximum):
+    result=None
+    if 1==maximum:
+        result=1
+    else:
+        for i in range(1, maximum+1):
+            newNumber=number-i
+            if (newNumber>=0):
+                result+=differentWaysWithMaximum(newNumber, i)
+    #print (number, maximum, result)
+    return result
 
+def differentWays(number):
+    return differentWaysWithMaximum(number, number)
+
+print differentWays(5)
