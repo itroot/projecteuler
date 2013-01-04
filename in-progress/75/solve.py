fb2d128b30c575abb7e46d896abd6939c0ff52b8
Ivan Tolstosheyev <itroot@yandex-team.ru>
2013-01-04 14:31:54 +0400

    [#75] Rewrite

--- in-progress/75/solve.py
+++ in-progress/75/solve.py
@@ -2,59 +2,50 @@
 # -*- coding: utf-8 -*-
 
 """
-http://en.wikipedia.org/wiki/Pythagorean_triple#Generating_a_triples
+http://en.wikipedia.org/wiki/Tree_of_primitive_Pythagorean_triples
 """
-import sys
-sys.path.append("../lib")
-import Factorize
-import math
 
-def isGeneratingPrimitiveTriple(m, n):
-    if (n>m):
-        (m, n)=(n, m)
-    difference=m-n
-    if (0==difference%2):
-        return False
-    if 1==n:
-        return True
-    f=Factorize.Factorization()
-    nFactors=set(f.factorize(n))
-    mFactors=set(f.factorize(m))
-    return 0==len(mFactors.intersection(nFactors))
+allTriples=[]
+maxWireLength=1500000
+pairMN=(2, 1)
 
-#print isGeneratingPrimitiveTriple(21, 3)
-#sys.exit(0)
-maxWireLength=1000#1500000
+def nextLevel(pairMN):
+    (m, n)=pairMN
+    return ((2*m-n, m), (2*m+n, m), (m+2*n, n))
 
-maxHypoLength=int(math.ceil(maxWireLength/2))
-maxGenerativeNumber=int(math.ceil(math.sqrt(maxHypoLength)))
-#print maxGenerativeNumber
+def MN2ABC(pairMN):
+    (m, n)=pairMN
+    return (m**2-n**2, 2*m*n, m**2+n**2)
 
-#tripleList=[]
-sumList=[]
-result=0
-for m in range(1, maxGenerativeNumber+1):
-    print m
-    for n in range(1, m):
-        a=m**2-n**2
-        b=2*m*n
-        c=m**2+n**2
-        if (a+b+c)<=maxWireLength and isGeneratingPrimitiveTriple(m, n):
-            #tripleList.append((a, b, c))
-            sumList.append(a+b+c)
-            result+=1
-#print tripleList
-sumList=sorted(sumList)
-uniqueList=[sumList[0]]
-for (i, number) in enumerate(sumList):
-    print i
-    isUnique=True
-    for uniqueNumber in uniqueList:
-        if number%uniqueNumber==0:
-            isUnique=False
-            break
-    if isUnique:
-        uniqueList.append(number)
-#print sumList
-print uniqueList, len(uniqueList)
-#print result
+def traverse(pairMN, condition):
+    if (condition(pairMN)):
+        threePairMN=nextLevel(pairMN)
+        for pairMN in threePairMN:
+            traverse(pairMN, condition)
+
+def traverseCondition(pairMN):
+    triple=MN2ABC(pairMN)
+    result=sum(triple)<=maxWireLength
+    if result:
+        allTriples.append(triple)
+    return result
+
+traverse(pairMN, traverseCondition)
+
+def uniqifyFactor(lengthList):
+    uniqueList=[]
+    for number in lengthList:
+        isUnique=True
+        for uniqueNumber in uniqueList:
+            if 2*uniqueNumber>number:
+                break
+            if number%uniqueNumber==0:
+                isUnique=False
+                break
+        if isUnique:
+            uniqueList.append(number)
+    return uniqueList
+
+lengthList=sorted(map(lambda e: sum(e), allTriples))
+uniqueLengthList=uniqifyFactor(lengthList)
+print len(uniqueLengthList)
