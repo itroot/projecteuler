9df0f454689e6b870b458bdd90bb9d270c90840c
Ivan Tolstosheyev <itroot@yandex-team.ru>
2013-01-05 18:08:44 +0400

    [#78] Reimplement JFF

--- in-progress/78/solve.py
+++ in-progress/78/solve.py
@@ -2,29 +2,31 @@
 # -*- coding: utf-8 -*-
 
 """
-http://en.wikipedia.org/wiki/Partition_(number_theory)#Intermediate_function
+http://ru.wikipedia.org/wiki/%D0%A0%D0%B0%D0%B7%D0%B1%D0%B8%D0%B5%D0%BD%D0%B8%D0%B5_%D1%87%D0%B8%D1%81%D0%BB%D0%B0#.D0.A0.D0.B5.D0.BA.D1.83.D1.80.D1.80.D0.B5.D0.BD.D1.82.D0.BD.D1.8B.D0.B5_.D1.84.D0.BE.D1.80.D0.BC.D1.83.D0.BB.D1.8B
 """
 
 import math
 
 cache={}
 
-def p(k, n):
-    if k>n:
-        return 0
-    elif k==n:
+def P(n, k):
+    if n==0 and k==0:
         return 1
+    elif k==0:
+        return 0
     else:
-        nk=str(n)+"/"+str(k)
-        if nk in cache:
-            return cache[nk]
+        kn=(k, n)
+        if kn in cache:
+            return cache[kn]
+        if k<=n:
+            result=P(n-k, k)+P(n, k-1)
         else:
-            result=p(k+1, n)+p(k, n-k)
-            cache[nk]=result
-            return result
+            result=P(n, n)
+        cache[kn]=result
+        return result
 
 def partitions(n):
-    return 1+sum(map(lambda k: p(k, n-k), range(1, int(math.floor(n/2)))))
+    return P(n, n)
 
-for i in range(1, 100):
-    print partitions(i)
+for i in range(1, 101):
+    print i, partitions(i)
