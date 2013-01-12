1fe78f277e58a6d3c5ef3304f88de4c4239043a3
Ivan Tolstosheyev <itroot@yandex-team.ru>
2013-01-12 19:54:15 +0400

    [#81] Add convertor from matrix to graph
    In fact, this is not proper convertor, because we do not count
    starting node weight. It's better to invert our graph,
    but I'l try to do it hacky way.

--- in-progress/81/solve.py
+++ in-progress/81/solve.py
@@ -1,2 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+import pprint
+
+def loadMatrix(fileName):
+    data=open(fileName).read()
+    return map(lambda e: e.split(" "), data.strip("\n").split("\n"))
+
+def convertMatrixToGraph(matrix, down=True, right=True):
+    vertices=[]
+    edges=[]
+    dimension=len(matrix)
+    for i in range(0, dimension):
+        for j in range(0, dimension):
+            vertices.append((i, j))
+            if (down and i+1<dimension):
+                edges.append(((i, j), (i+1, j), matrix[i+1][j]))
+            if (right and j+1<dimension):
+                edges.append(((i, j), (i, j+1), matrix[i][j+1]))
+    return (vertices, edges)
+
+matrix=loadMatrix("matrix-test.txt")
+graph=convertMatrixToGraph(matrix)
+pprint.pprint(graph)
