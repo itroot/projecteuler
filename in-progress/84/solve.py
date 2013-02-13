ade601d2d490b4ecf500652ec0f05271ea6f066e
Ivan Tolstosheyev <itroot@yandex-team.ru>
2013-02-13 21:24:47 +0400

    [#84] Make code clean. A bit cleaner.

--- in-progress/84/solve.py
+++ in-progress/84/solve.py
@@ -9,7 +9,7 @@ board=[
 ]
 length=len(board)
 
-timesVisited=[0 for i in range(0, length)]
+timesVisited=[0]*length
 # timesVisited[0]=1
 
 def rollTheDice():
@@ -19,25 +19,39 @@ def rollTheDice():
 def rollTheDiceTwice():
     return (rollTheDice(), rollTheDice())
 
-# http://stackoverflow.com/questions/473973/shuffle-an-array-with-python
-def getCommunityChestCard():
-    import random
-    number=random.randrange(0, 16)
-    if (0==number):
-        return "2GO"
-    elif (1==number):
-        return "2JAIL"
-    else:
-        return "DONTMOVE"
+communityChestCardPile=["2GO", "2JAIL"]+(14*["DONTMOVE"])
+chanceCardPile=["2GO", "2JAIL", "2C1", "2E3", "2H2", "2R1", "2nR", "2nR", "2nU", "back3"]+(6*["DONTMOVE"])
 
-def getChanceCard():
+communityChestActions={
+    "2JAIL" : (lambda position: 10),
+    "2GO" : (lambda position: 0),
+    "DONTMOVE" : (lambda position: position),
+}
+
+def moveToNext(position, sortedPointList):
+    for point in sortedPointList:
+        if position<point:
+            return point
+    return sortedPointList[0]
+
+chanceActions={
+    "2JAIL" : (lambda position: 10),
+    "2GO" : (lambda position: 0),
+    "2C1" : (lambda position: 11),
+    "2E3" : (lambda position: 24),
+    "2H2" : (lambda position: 39),
+    "2R1" : (lambda position: 5),
+    "2nR" : (lambda position: moveToNext(position, (5, 15, 25, 35))),
+    "2nU" : (lambda position: moveToNext(position, (12, 18))),
+    "back3" : (lambda position: (position-3+length)%length),
+    "DONTMOVE" : (lambda position: position),
+}
+
+# http://stackoverflow.com/questions/473973/shuffle-an-array-with-python
+def getRandomCardFromPile(pile):
     import random
-    cardList=["2GO", "2JAIL", "2C1", "2E3", "2H2", "2R1", "2nR", "2nR", "2nU", "back3"]
-    number=random.randrange(0, 16)
-    if number<len(cardList):
-        return cardList[number]
-    else:
-        return "DONTMOVE"
+    number=random.randrange(0, len(pile))
+    return pile[number]
 
 class GameState:
     position=0
@@ -58,31 +72,11 @@ class GameState:
             if (label=="G2J"):
                 self.position=10
             elif (label.startswith("CC")):
-                communityChestCard=getCommunityChestCard()
-                self.position={
-                    "2JAIL" : (lambda position: 10),
-                    "2GO" : (lambda position: 0),
-                    "DONTMOVE" : (lambda position: position),
-                }[communityChestCard](self.position)
+                communityChestCard=getRandomCardFromPile(communityChestCardPile)
+                self.position=communityChestActions[communityChestCard](self.position)
             elif (label.startswith("CH")):
-                def moveToNext(position, sortedPointList):
-                    for point in sortedPointList:
-                        if position<point:
-                            return point
-                    return sortedPointList[0]
-                chanceCard=getChanceCard()
-                self.position={
-                    "2JAIL" : (lambda position: 10),
-                    "2GO" : (lambda position: 0),
-                    "2C1" : (lambda position: 11),
-                    "2E3" : (lambda position: 24),
-                    "2H2" : (lambda position: 39),
-                    "2R1" : (lambda position: 5),
-                    "2nR" : (lambda position: moveToNext(position, (5, 15, 25, 35))),
-                    "2nU" : (lambda position: moveToNext(position, (12, 18))),
-                    "back3" : (lambda position: (position-3+length)%length),
-                    "DONTMOVE" : (lambda position: position),
-                }[chanceCard](self.position)
+                chanceCard=getRandomCardFromPile(chanceCardPile)
+                self.position=chanceActions[chanceCard](self.position)
         timesVisited[self.position]+=1
 
 experimentNumber=10**5  
