1d991fe27a0959a791c77659c9e740826c76dcd5
Ivan Tolstosheyev <itroot@yandex-team.ru>
2013-05-21 10:24:45 +0400

    [#96] Add sudoku solver for simple cases

--- in-progress/96/solve.py
+++ in-progress/96/solve.py
@@ -15,11 +15,69 @@ def readSudokuFile(filename):
         result.append(sudoku)
     return result
 
+"""
+def findEmptyPosition(sudoku):
+    for (y, row) in enumerate(sudoku):
+        for (x, digit) in enumerate(row):
+            if digit==0:
+                return (x, y)
+    return None
+"""
+
+def getSquareDigits(sudoku, position):
+    (xOrigin, yOrigin)=position
+    x=(xOrigin/3)*3
+    y=(yOrigin/3)*3
+    squareSize=3
+    squareRange=range(0, squareSize)
+    result=[]
+    for yRange in squareRange:
+        for xRange in squareRange:
+            result.append(sudoku[y+yRange][x+xRange])
+    return result
+
+def getUsedDigits(sudoku, position):
+    (x, y)=position
+    yDigits=sudoku[y]
+    xDigits=map(lambda row: row[x], sudoku)
+    squareDigits=getSquareDigits(sudoku, position)
+    #print "xDigits", xDigits
+    #print "yDigits", yDigits
+    #print "squareDigits", squareDigits
+    return set(xDigits+yDigits+squareDigits)-set([0])
+
+
 def solveSudoku(sudoku):
     # on every 0 insert possible digit and continue
-    print sudoku
+    #import pprint
+    #pprint.pprint(sudoku)
+    #position=findEmptyPosition(sudoku)
+    stats=[]
+    for (y, row) in enumerate(sudoku):
+        for (x, digit) in enumerate(row):
+            if digit==0:
+                stats.append(((x, y), getUsedDigits(sudoku, (x, y))))
+    if len(stats)==0:
+        return sudoku
+    stats=sorted(stats, reverse=True, key=lambda e: len(e[1]))
+    print stats[0]
+    (position, digitSet)=stats[0]
+    remainingDigits=set(range(1, 10))-digitSet
+    if len(remainingDigits)!=1:
+        raise Exception("Not implemented yet")
+    (x, y)=position
+    sudoku[y][x]=list(remainingDigits)[0]
+    return solveSudoku(sudoku)
+    #usedDigits=getUsedDigits(sudoku, position)
+    #print usedDigits
+    #(x, y)=position
+    #print (x, y)
 
 sudokuList=readSudokuFile("sudoku.txt")
 
+
 for sudoku in sudokuList:
-    solveSudoku(sudoku) 
+    solvedSudoku=solveSudoku(sudoku)
+    import pprint
+    pprint.pprint(solvedSudoku)
+    break
