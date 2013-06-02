cc38bef79e3c686ae792ae61af7cb71e347fbd3a
Ivan Tolstosheyev <itroot@yandex-team.ru>
2013-06-02 16:32:21 +0400

    [#100] Start to solve

---
+++ in-progress/100/solve.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import math
+
+lowerLimit=10**12
+blueDiscNumber=int(lowerLimit*math.sqrt(0.5))
+
+print blueDiscNumber**2
+print (blueDiscNumber+1)**2
