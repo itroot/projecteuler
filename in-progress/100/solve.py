21f34618d88eb36fbb79f1cff5cf6f45e98e0421
Ivan Tolstosheyev <itroot@yandex-team.ru>
2013-06-15 20:04:29 +0400

    [#100] Add fractions test

--- in-progress/100/solve.py
+++ in-progress/100/solve.py
@@ -2,9 +2,27 @@
 # -*- coding: utf-8 -*-
 
 import math
+import fractions
 
 lowerLimit=10**12
 blueDiscNumber=int(lowerLimit*math.sqrt(0.5))
 
 print blueDiscNumber**2
 print (blueDiscNumber+1)**2
+
+def fractionize(blueDiscNumber, totalNumber):
+    result=fractions.Fraction(blueDiscNumber, totalNumber)
+    result=result*fractions.Fraction(blueDiscNumber-1, totalNumber-1)
+    return result
+
+number=lowerLimit
+numberLimit=(blueDiscNumber+1)**2
+print numberLimit
+while number<numberLimit:
+    result=fractionize(blueDiscNumber, number)
+    #print result
+    if (result.numerator==1 and result.denominator==2):
+        break
+    number+=1
+
+print fractionize(blueDiscNumber, lowerLimit)
