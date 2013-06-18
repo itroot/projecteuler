4bbfc9b7e12f4f95b70ae7059b09dee066569fe7
Ivan Tolstosheyev <itroot@yandex-team.ru>
2013-06-18 12:10:20 +0400

    [#90] Solve problem

--- solved/level-4/90/solve.py
+++ solved/level-4/90/solve.py
@@ -8,6 +8,7 @@ squareNumberList = [x**2 for x in range(1, 10)]
 squareDigitList = map(lambda e: (e/10, e%10), squareNumberList)
 #print squareDigitList, len(squareDigitList)
 
+"""
 def splitList(l, number):
     left = []
     right = []
@@ -29,7 +30,14 @@ def cleanSet(st):
         st.remove(6)
     if 9 in st:
         st.remove(9)
+"""
 
+def expandSet(numberSet):
+    if 9 in numberSet:
+        numberSet.add(6)
+    if 6 in numberSet:
+        numberSet.add(9)
+"""
 for i in range(0, 2**len(squareDigitList)):
     (left, right) = splitList(squareDigitList, i)    
     oneCubeDigits = set(map(lambda e: e[0], left) + map(lambda e: e[1], right))
@@ -38,12 +46,29 @@ for i in range(0, 2**len(squareDigitList)):
     cleanSet(secondCubeDigits)
     print oneCubeDigits , secondCubeDigits
         #print oneCubeDigits, secondCubeDigits
-
 """
+#"""
 cubeCombinations = list(combinations(range(0, 10), 6))
 # todo extend 6+9
+result=[]
 for cube1 in cubeCombinations:
+    cube1set = set(cube1)
+    expandSet(cube1set)
     for cube2 in cubeCombinations:
-        pass
-        #print cube1, cube2
-"""
+        cube2set = set(cube2)
+        expandSet(cube2set)
+        found = True
+        for (digit1, digit2) in squareDigitList:
+            if not ((digit1 in cube1set and digit2 in cube2set) or (digit1 in cube2set and digit2 in cube1set)):
+                found = False
+                break
+        if (found):
+            result.append((cube1, cube2))
+result = map(lambda e: (tuple(sorted(e[0])), tuple(sorted(e[1]))), result)
+result = map(lambda e: {True: e, False: (e[1], e[0])}[e[1]>e[0]], result)
+result = list(set(result))
+#print result
+#import pprint
+#pprint.pprint(result)
+print len(result)
+#"""
