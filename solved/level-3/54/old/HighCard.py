1dcb4a2d50b189801a391a2752d2238db360889e
Ivan Tolstosheyev <itroot@yandex-team.ru>
2013-06-28 10:40:46 +0400

    [#54] Remove old code

--- solved/level-3/54/old/HighCard.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from Combination import *
-
-class HighCard(Combination):
-    def __init__(self):
-        Combination.__init__(self)
-    def classify(self, cards):
-        return tuple(["HighCard"]+sorted(map(lambda e: e.rank(), cards), reverse=True))
