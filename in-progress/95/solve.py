c4a6674a476d09b3dff5094412a5f7e65e32f3dc
Ivan Tolstosheyev <itroot@yandex-team.ru>
2013-06-29 08:47:19 +0400

    Get all divisors list

--- in-progress/95/solve.py
+++ in-progress/95/solve.py
@@ -4,6 +4,7 @@
 import pprint
 
 # draw DAG graph for numbers < 100
+# you can code any dag in numbers, any new dependnecy - just multiplication
 
 upperLimit = 10**3+1
 #upperLimit = 10**6+1
@@ -28,14 +29,20 @@ def getPrimeDivisors(number):
     else:
         return [divisorList[0]]+getPrimeDivisors(divisorList[1])
 
-"""
+#"""
 import sys
 sys.path.append("../lib")
-from Factorize import getAllDivisors
-"""
+from Factorize import AllDivisors
+def divisors(factorization):
+    return AllDivisors(factorization).divisors()
+
 # 2,2,2 -> 2,4,8 , choose one
 # 3, 5 -> choose any
-primeDivisors = map(getPrimeDivisors, range(0, upperLimit))
+# (2, 3), (3, 1), (5, 2) -> (3+1)*(1+1)(2+1)
+# (2, 4, 8), (3), (5, 25)
 
-pprint.pprint(sieve[:100])
-pprint.pprint(primeDivisors[:100])
+
+allDivisorsList = map(lambda e: divisors(getPrimeDivisors(e)), range(0, upperLimit))
+
+#pprint.pprint(sieve[:100])
+pprint.pprint(allDivisorsList[:100])
