41da9ce649f71b4c1d30496d81d23b48ec8de2cb
Ivan Tolstosheyev <itroot@yandex-team.ru>
2013-07-11 21:13:54 +0400

    Move cpp.launch code to module

---
+++ lib/cpp.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+def compile():
+    import subprocess
+    import os
+    secretPath = ".pycpplauncher"
+    if not os.path.exists(secretPath):
+        os.mkdir(secretPath)
+    data = open("solve.cpp").read()
+    import hashlib
+    sha1 = hashlib.sha1()
+    sha1.update(data)
+    digest = sha1.hexdigest()
+    binaryPath = secretPath+"/"+digest
+    if (not os.path.exists(secretPath+"/"+digest)):
+        subprocess.check_call("g++ -std=c++0x -g -O0 -Wall solve.cpp -o %s" % binaryPath, shell=True)
+    if os.path.exists("solve"):
+        os.remove("solve")
+    os.symlink(binaryPath, "solve")
+
+def launch():
+    compile()
+    import subprocess
+    subprocess.check_call("./solve", shell=True)
--- solved/level-5/102/solve.py
+++ solved/level-5/102/solve.py
@@ -1,30 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-def compile():
-    import subprocess
-    import os
-    secretPath = ".pycpplauncher"
-    if not os.path.exists(secretPath):
-        os.mkdir(secretPath)
-    data = open("solve.cpp").read()
-    import hashlib
-    sha1 = hashlib.sha1()
-    sha1.update(data)
-    digest = sha1.hexdigest()
-    binaryPath = secretPath+"/"+digest
-    if (not os.path.exists(secretPath+"/"+digest)):
-        subprocess.check_call("g++ -std=c++0x -g -O0 -Wall solve.cpp -o %s" % binaryPath, shell=True)
-    if os.path.exists("solve"):
-        os.remove("solve")
-    os.symlink(binaryPath, "solve")
+import sys
+sys.path.append("../lib")
+from cpp import launch
 
-def launch():
-    import subprocess
-    subprocess.check_call("./solve", shell=True)
-
-def loadCpp():
-    compile()
-    launch()
-
-loadCpp()
+launch()
