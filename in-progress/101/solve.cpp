fbd5930aad5118c98aede52a0ff08ba2bdcdad27
Ivan Tolstosheyev <itroot@yandex-team.ru>
2013-07-14 18:36:35 +0400

    [#101] Add comment

--- in-progress/101/solve.cpp
+++ in-progress/101/solve.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
 
+/**
+ * How's approximation works
+ * you have list of variables (x_i, y_i), you get a polynome as
+ * sum for each j and i !=j : ((x-x_i) .. xj/(x_i - xj)) * yi ...  == 0, 
+ * to make coefs integer, we can multiply.
+ * see : http://en.wikipedia.org/wiki/Polynomial_interpolation
+ * 
+ * I think is will be easear to do in python
+ */
+
 int main(int argc, char* argv[])
 {
     std::cout << "test" << std::endl;
