f3cab08aeb06b121d6746d753d77d88e5ec6631d
Ivan Tolstosheyev <itroot@yandex-team.ru>
2013-07-14 17:49:48 +0400

    Fix yaml handling

--- lib/cpp.py
+++ lib/cpp.py
@@ -2,7 +2,12 @@
 
 def readMeta():
     import yaml
-    return yaml.load(open("solve.yaml"))
+    import os
+    path = "solve.yaml"
+    if os.path.exists(path):
+        return yaml.load(open(path))
+    else:
+        return {}
 
 def profile(handler):
     #try:
@@ -24,7 +29,9 @@ def compile():
     binaryPath = secretPath+"/"+digest
     if (not os.path.exists(secretPath+"/"+digest)):
         meta = readMeta()
-        lstring = " ".join(map(lambda e: "-l"+e, meta["libraries"]))
+        lstring = ""
+        if "libraries" in meta:
+            lstring = " ".join(map(lambda e: "-l"+e, meta["libraries"]))
         template = "g++ -std=c++0x -g -O0 -Wall solve.cpp %s -o %s"
         command = template % (lstring, binaryPath)
         subprocess.check_call(command, shell=True)
