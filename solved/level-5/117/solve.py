1fb77392b4501b39f9be6c212608f3418a9c6147
Ivan Tolstosheyev <itroot@yandex-team.ru>
2013-12-29 18:55:46 +0400

    [#117] Make code easer to understand

--- solved/level-5/117/solve.py
+++ solved/level-5/117/solve.py
@@ -5,10 +5,12 @@ import sys
 sys.path.append("../lib")
 from OwnMemoize import Memoize
 
+minTile = 2
+maxTile = 4
+
 upperLimit = 50
 
-def rgb_n_tiles(callee, number, i, n):
-    r = i + n
+def rgb_n_tiles(callee, number, r):
     if not r > number:
         return callee(callee, number - r)
     else:
@@ -16,10 +18,10 @@ def rgb_n_tiles(callee, number, i, n):
 
 def rgb_tiles(callee, number):
     result = 1
-    if number<2:
+    if number<minTile:
         return result
     for i in range(0, number):
-        result += sum(map(lambda e: rgb_n_tiles(callee, number, i, e), range(2,5)))
+        result += sum(map(lambda e: rgb_n_tiles(callee, number, i + e), range(minTile, maxTile + 1)))
     return result
 
 m = Memoize()
