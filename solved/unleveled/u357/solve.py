a991145302ccc398d02b249fc66d3cc30de0d0fa
Ivan Tolstosheyev <itroot@yandex-team.ru>
2017-01-11 12:29:28 +0300

    Upgrade data

--- solved/unleveled/u357/solve.py
+++ solved/unleveled/u357/solve.py
@@ -13,13 +13,21 @@ upper_limit = 10 ** 7
 f11n = F11n()
 
 primes = map(int, open("../lib/data/primes-100000000.list").read().split())
+primes_set = frozenset(primes)
+
+result = 0
 
 for p in primes[1:]:
     i = p - 1
+    if not (i/2 + 2) in primes_set:
+        continue
     factors = f11n.factorize(i)
     ad = AD(factors)
     divisors = ad.divisors()
     blahs = list(set(map(lambda (d1, d2): d1 + d2 , zip(divisors, reversed(divisors)))))
     #print i, blahs
-    if all(map(lambda e: len(f11n.factorize(e)) == 1, blahs)):
+    if all(map(lambda e: e in primes_set, blahs)):
         print i, factors, divisors, blahs
+        result += i
+
+print result
