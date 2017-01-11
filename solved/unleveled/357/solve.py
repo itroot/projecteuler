1875019efb283b2e2ea073537d6af7c98ecb371f
Ivan Tolstosheyev <itroot@yandex-team.ru>
2017-01-11 15:57:04 +0300

    Solve #357

--- solved/unleveled/357/solve.py
+++ solved/unleveled/357/solve.py
@@ -7,7 +7,7 @@ sys.path.append("../lib")
 from Factorize import Factorization as F11n
 from Factorize import AllDivisors as AD
 
-upper_limit = 10 ** 7
+upper_limit = 10 ** 8
 #upper_limit = 10 ** 3
 
 f11n = F11n()
@@ -16,6 +16,20 @@ primes = map(int, open("../lib/data/primes-100000000.list").read().split())
 primes_set = frozenset(primes)
 
 result = 0
+
+"""
+o = open("../lib/data/minfactor-100000000.list", "w")
+for i in xrange(upper_limit):
+    if i in primes_set:
+        o.write("%d %d\n" % (i, i))
+    else:
+        for p in primes:
+            if i % p == 0:
+                o.write("%d %d\n" % (i, p))
+                break
+o.close()
+#sys.exit(0)
+"""
 
 for p in primes[1:]:
     i = p - 1
@@ -30,4 +44,4 @@ for p in primes[1:]:
         print i, factors, divisors, blahs
         result += i
 
-print result
+print result + 1
